844b17b63d6626f4de16a6da698cfc31905a459a
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-09-15 00:23:34 -0400

    Changed javascript methods

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-
 window.onload = function () {
     var genQuoteButton = $("#genQuoteButton");
     var quote = $("#quoteBox");
 
     genQuoteButton.click(function() {
-        quote.empty()
-             .append("TEST");        
+        quote.html("TEST");        
     });
 }
 
