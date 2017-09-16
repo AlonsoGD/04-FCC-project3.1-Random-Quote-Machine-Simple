4c56086c336fd8465ca2a96f14154690441f748c
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-09-16 21:00:06 +0200

    trying with ajax

--- js/script.js
+++ js/script.js
@@ -6,8 +6,16 @@ window.onload = function () {
         quote.html("TEST");        
     });
 
-    var apiEndPoint = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
-    $.ajax(apiEndPoint);
+    var apiEndPointURL = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+    //var callbackURL = "";
+    
+    var response = $.ajax({
+			type: "GET",
+			url: apiEndPointURL,
+			dataType: 'JSONP'});
+			success: alert("Loaded!");
+			
+					
 };
-//TEST CONSOLE IN LINUX
-  
+
+ 
