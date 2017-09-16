36553263bf2db9b8e49b9333e96185b2f5ff1578
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-09-16 13:02:48 -0400

    Adding API

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ window.onload = function () {
     genQuoteButton.click(function() {
         quote.html("TEST");        
     });
-}
 
+    var apiEndPoint = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+    $.ajax(apiEndPoint);
+};
+    
   
